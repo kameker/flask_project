1b5210ac8db86776f428042cf731bb399e81f5aa
kameker <vsrnpra2@gmail.com>
2022-04-13 22:05:25 +0300

    15

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/data.db")
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 @app.route('/')
