039b27d0e4e163f61c8a000a3b560774f891009c
kameker <vsrnpra2@gmail.com>
2022-04-13 22:03:27 +0300

    14

--- data/users.py
+++ data/users.py
@@ -13,6 +13,7 @@ class User(SqlAlchemyBase):
     about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String,
                               index=True, unique=True, nullable=True)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from os import environ
 from smtplib import SMTP
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -18,8 +17,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/data.db")
-    port = int(environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
 
 
 @app.route('/')
@@ -31,22 +29,37 @@ def _():
 def reqister():
     form = RegisterForm()
     if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
+        msg = MIMEMultipart()
+        server = SMTP('smtp.gmail.com: 587')
+        if validate(form.email.data):
+            if form.password.data != form.password_again.data:
+                return render_template('register.html', title='Регистрация',
+                                       form=form,
+                                       message="Пароли не совпадают")
+            db_sess = db_session.create_session()
+            if db_sess.query(User).filter(User.email == form.email.data).first():
+                return render_template('register.html', title='Регистрация',
+                                       form=form,
+                                       message="Такой пользователь уже есть")
+            user = User(
+                name=form.name.data,
+                email=form.email.data,
+                password=form.password.data
+            )
+            user.set_password(form.password.data)
+            db_sess.add(user)
+            db_sess.commit()
+            message = 'Вы зарегистрировались'
+            msg.attach(MIMEText(message, 'plain'))
+            server.starttls()
+            server.login(from_email, password)
+            server.sendmail(from_email, form.email.data, msg.as_string())
+            server.quit()
+            return redirect('/login')
+        else:
             return render_template('register.html', title='Регистрация',
                                    form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        user = User(
-            name=form.password.data,
-            email=form.email.data,
-        )
-        print(f"{form.password.data} - {form.email.data}")
-        user.set_password(form.password.data)
-        return redirect('/login')
+                                   message="Такого адреса не существует")
     return render_template('register.html', title='Регистрация', form=form)
 
 
